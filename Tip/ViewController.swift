0e74a031ff5376970a08d8600414c2f4247b1420
Samara <swondimu10@gmail.com>
2020-07-10 20:46:29 -0400

    Added sliding bar feature to change tip amount Improved UI design

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -14,29 +14,51 @@ class ViewController: UIViewController {
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipPercentageLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControlSlider: UISlider!
+    @IBOutlet weak var tipTotal: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.billAmountTextField.keyboardType = UIKeyboardType.decimalPad
+        self.totalLabel.adjustsFontSizeToFitWidth = true
         // Do any additional setup after loading the view.
     }
 
+   
     @IBAction func onTap(_ sender: Any) {
     }
     @IBAction func calculateTip(_ sender: Any) {
         //get initial bill amount and calculate tips
-        let bill = Double(billAmountTextField.text!) ?? 0
-        let tipPercentages = [0.15, 0.18, 0.2]
+        let bill = Float(billAmountTextField.text!) ?? 0
         
         
         //calculate tip and total
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var tipPercent = Int(tipControlSlider.value)
+        let tip = bill * (Float(tipPercent) / 100)
         let total = bill + tip
         
         //update the tip and total labels
-        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        tipPercentageLabel.text = String(format: "%i",Int(tipPercent))
         totalLabel.text = String(format: "$%.2f", total)
+        tipTotal.text = String(format: "$%.2f", tip)
     }
-    
+    @IBAction func onSlide(_ sender: Any) {
+        //get initial bill amount and calculate tips
+        let bill = Float(billAmountTextField.text!) ?? 0
+       
+       
+        //calculate tip and total
+       
+        var tipPercent = Int(tipControlSlider.value)
+        let tip = bill * (Float(tipPercent) / 100)
+        let total = bill + tip
+       
+        //update the tip and total labels
+        tipPercentageLabel.text = String(format: "%i",Int(tipPercent))
+        totalLabel.text = String(format: "$%.2f", total)
+        tipTotal.text = String(format: "$%.2f", tip)
+    }
 }
 
